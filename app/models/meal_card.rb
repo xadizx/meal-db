aca8f40eaf00660af3bf34288aaef98fae4fb182
Adam Zabraniak <adam.z@ragnarson.com>
2025-07-28 14:20:02 +0200

    fix issue with meal_thumbnails

--- app/models/meal_card.rb
+++ app/models/meal_card.rb
@@ -7,6 +7,6 @@ end
 class MealCard < Dry::Struct
   attribute :external_id, Types::Integer
   attribute :name, Types::String
-  attribute :thumbnail_image_url, Types::String
+  attribute :thumbnail_image_url, Types::String.optional
   attribute :category_name, Types::String
 end
